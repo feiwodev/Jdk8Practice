b8eaa66848256babb377719fb6535f0a166d64e6
feiwo <zhuyongluck@qq.com>
2019-09-09 23:23:13 +0800

    predicate demo

--- src/main/java/xyz/feiwo/jdk8/PredicateDemo.java
+++ src/main/java/xyz/feiwo/jdk8/PredicateDemo.java
@@ -1,5 +1,7 @@
 package xyz.feiwo.jdk8;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 /**
@@ -10,6 +12,49 @@ public class PredicateDemo {
 
         Predicate<String> predicate = p -> p.length() == 5;
         System.out.println(predicate.test("Hello"));
+        System.out.println("-------------------");
 
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        // 过滤偶数
+        conditionFilter(list,item -> item % 2 == 0);
+        System.out.println("-------------------");
+
+        // 奇数
+        conditionFilter(list, item -> item % 2 != 0);
+        System.out.println("-------------------");
+
+        // 大于5的数
+        conditionFilter(list,item -> item > 5);
+        System.out.println("-------------------");
+
+        // 小于3的数
+        conditionFilter(list, item -> item < 3);
+        System.out.println("-------------------");
+
+        // 打印全部的数
+        conditionFilter(list, item -> true);
+        System.out.println("-------------------");
+
+        // 大于5的偶数
+        conditionFilter2(list,item -> item > 5, item -> item % 2 == 0);
+
+    }
+
+    private static void conditionFilter(List<Integer> list, Predicate<Integer> predicate) {
+        for (Integer integer : list) {
+            if (predicate.test(integer)) {
+                System.out.println(integer);
+            }
+        }
+    }
+
+    // predicate 默认方法 and , or , (negate,not) ,isEqual
+    private static void conditionFilter2(List<Integer> list,Predicate<Integer> predicate, Predicate<Integer> predicate2) {
+        for (Integer integer : list) {
+            if (predicate.and(predicate2).test(integer)) {
+                System.out.println(integer);
+            }
+        }
     }
 }
